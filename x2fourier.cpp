eeb5d860624f17873b73addf5fb282db501518ca
Josh Smith <jdsmit60@users.noreply.github.com>
2017-02-22 10:18:56 -0700

    Switch from i to x
    Changed variable i to x so that things are more mathematically familiar

--- x2fourier.cpp
+++ x2fourier.cpp
@@ -16,15 +16,15 @@ int main(void) {
 	L = 10;
 	upperLimit = 500; // theoretically would be infinity for a perfect representation of the function
 
-	for(double i=domainStart; i <= domainEnd; i+=domainStep) {
+	for(double x=domainStart; x <= domainEnd; x+=domainStep) {
 		
 		latestY = 0;
 		
 		for(int n=1; n <= upperLimit; n++) {
-			latestY += (/* start An */ (L*L*((2*M_PI*M_PI*n*n-1)*sin(2*M_PI*n)+2*M_PI*n*cos(2*M_PI*n)))/(2*pow(M_PI,3)*pow(n,3)) /* end An */)*cos(n*M_PI*i/L) + (/* start Bn */ (L*L*((1-2*M_PI*M_PI*n*n)*cos(2*M_PI*n)+2*M_PI*n*sin(2*M_PI*n)-1))/(3*pow(M_PI,3)*pow(n,3)) /* end Bn */)*sin(n*M_PI*i/L);
+			latestY += (/* start An */ (L*L*((2*M_PI*M_PI*n*n-1)*sin(2*M_PI*n)+2*M_PI*n*cos(2*M_PI*n)))/(2*pow(M_PI,3)*pow(n,3)) /* end An */)*cos(n*M_PI*x/L) + (/* start Bn */ (L*L*((1-2*M_PI*M_PI*n*n)*cos(2*M_PI*n)+2*M_PI*n*sin(2*M_PI*n)-1))/(3*pow(M_PI,3)*pow(n,3)) /* end Bn */)*sin(n*M_PI*x/L);
 		}
 
-		cout << setprecision(20) << i << " " << latestY << endl;
+		cout << setprecision(20) << x << " " << latestY << endl;
 
 	}
 	return 0;
