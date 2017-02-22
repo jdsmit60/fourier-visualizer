cf2aaa41ec2bb88f7b19adf48e3287aef4c64ddb
Josh Smith <jdsmit60@users.noreply.github.com>
2017-02-22 10:24:12 -0700

    Switch from i to x
    Changed variable i to x so that things are more mathematically familiar

--- xfourier.cpp
+++ xfourier.cpp
@@ -16,15 +16,15 @@ int main(void) {
 	L = 8;
 	upperLimit = 100; // theoretically would be infinity for a perfect representation of the function
 
-	for(double i=domainStart; i <= domainEnd; i+=domainStep) {
+	for(double x=domainStart; x <= domainEnd; x+=domainStep) {
 		
 		latestY = 0;
 		
 		for(int n=1; n <= upperLimit; n++) {
-			latestY += (/* start An */ (2*L*(M_PI*n*sin(M_PI*n)+cos(M_PI*n)-1))/(M_PI*M_PI*n*n) /* end An */)*cos(n*M_PI*i/L) + (/* start Bn */ (2*L*(sin(M_PI*n)-M_PI*n*cos(M_PI*n)))/(M_PI*M_PI*n*n) /* end Bn */)*sin(n*M_PI*i/L);
+			latestY += (/* start An */ (2*L*(M_PI*n*sin(M_PI*n)+cos(M_PI*n)-1))/(M_PI*M_PI*n*n) /* end An */)*cos(n*M_PI*x/L) + (/* start Bn */ (2*L*(sin(M_PI*n)-M_PI*n*cos(M_PI*n)))/(M_PI*M_PI*n*n) /* end Bn */)*sin(n*M_PI*x/L);
 		}
 
-		cout << setprecision(20) << i << " " << latestY << endl;
+		cout << setprecision(20) << x << " " << latestY << endl;
 
 	}
 	return 0;
