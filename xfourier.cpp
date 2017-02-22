48fe07f9a61acbe7dd4082fe246cd48ba2855ae3
Josh Smith <jdsmit60@users.noreply.github.com>
2017-02-22 10:23:11 -0700

    Use M_PI
    Use cmath's builtin M_PI instead of declaring a constant PI

--- xfourier.cpp
+++ xfourier.cpp
@@ -9,7 +9,6 @@ int main(void) {
 	double domainStart, domainEnd, domainStep; // graph domain variables, used for x
 	long double latestY; // used for f(x)
 	int L, upperLimit; // equation variables
-	const double PI = 3.14159265358979323846264338;
 
 	domainStart = -20;
 	domainEnd = 20;
@@ -22,7 +21,7 @@ int main(void) {
 		latestY = 0;
 		
 		for(int n=1; n <= upperLimit; n++) {
-			latestY += (/* start An */ (2*L*(PI*n*sin(PI*n)+cos(PI*n)-1))/(PI*PI*n*n) /* end An */)*cos(n*PI*i/L) + (/* start Bn */ (2*L*(sin(PI*n)-PI*n*cos(PI*n)))/(PI*PI*n*n) /* end Bn */)*sin(n*PI*i/L);
+			latestY += (/* start An */ (2*L*(M_PI*n*sin(M_PI*n)+cos(M_PI*n)-1))/(M_PI*M_PI*n*n) /* end An */)*cos(n*M_PI*i/L) + (/* start Bn */ (2*L*(sin(M_PI*n)-M_PI*n*cos(M_PI*n)))/(M_PI*M_PI*n*n) /* end Bn */)*sin(n*M_PI*i/L);
 		}
 
 		cout << setprecision(20) << i << " " << latestY << endl;
