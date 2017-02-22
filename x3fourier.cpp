6ff9cf76ccf8644249847de5e14e9c5e2a6af2de
Josh Smith <jdsmit60@users.noreply.github.com>
2017-02-22 10:22:03 -0700

    Switch from i to x
    Changed variable i to x so that things are more mathematically familiar

--- x3fourier.cpp
+++ x3fourier.cpp
@@ -16,15 +16,15 @@ int main(void) {
 	L = 8;
 	upperLimit = 500; // theoretically would be infinity for a perfect representation of the function
 
-	for(double i=domainStart; i <= domainEnd; i+=domainStep) {
+	for(double x=domainStart; x <= domainEnd; x+=domainStep) {
 		
 		latestY = 0;
 		
 		for(int n=1; n <= upperLimit; n++) {
-			latestY += (/* start An */ (2*pow(L,3)*(M_PI*n*(M_PI*M_PI*n*n-6)*sin(M_PI*n)+3*(M_PI*M_PI*n*n-2)*cos(M_PI*n)+6))/(pow(M_PI,4)*pow(n,4)) /* end An */)*cos(n*M_PI*i/L) + (/* start Bn */ (-2*pow(L,3)*(M_PI*n*(n*n*M_PI*M_PI-6)*cos(M_PI*n)-3*(M_PI*M_PI*n*n-2)*sin(M_PI*n)))/(pow(M_PI,4)*pow(n,4)) /* end Bn */)*sin(n*M_PI*i/L);
+			latestY += (/* start An */ (2*pow(L,3)*(M_PI*n*(M_PI*M_PI*n*n-6)*sin(M_PI*n)+3*(M_PI*M_PI*n*n-2)*cos(M_PI*n)+6))/(pow(M_PI,4)*pow(n,4)) /* end An */)*cos(n*M_PI*x/L) + (/* start Bn */ (-2*pow(L,3)*(M_PI*n*(n*n*M_PI*M_PI-6)*cos(M_PI*n)-3*(M_PI*M_PI*n*n-2)*sin(M_PI*n)))/(pow(M_PI,4)*pow(n,4)) /* end Bn */)*sin(n*M_PI*x/L);
 		}
 
-		cout << setprecision(20) << i << " " << latestY << endl;
+		cout << setprecision(20) << x << " " << latestY << endl;
 
 	}
 	return 0;
