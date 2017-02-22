f631051eac9ed74e6b8c9144001a8339e340d7cd
Josh Smith <jdsmit60@users.noreply.github.com>
2017-02-22 10:17:17 -0700

    Use M_PI
    Use cmath's builtin M_PI instead of declaring a constant PI

--- x2fourier.cpp
+++ x2fourier.cpp
@@ -9,7 +9,6 @@ int main(void) {
 	double domainStart, domainEnd, domainStep; // graph domain variables, used for x
 	long double latestY; // used for f(x)
 	int L, upperLimit; // equation variables
-	const double PI = 3.14159265358979323846264338;
 
 	domainStart = -20;
 	domainEnd = 20;
@@ -22,7 +21,7 @@ int main(void) {
 		latestY = 0;
 		
 		for(int n=1; n <= upperLimit; n++) {
-			latestY += (/* start An */ (L*L*((2*PI*PI*n*n-1)*sin(2*PI*n)+2*PI*n*cos(2*PI*n)))/(2*pow(PI,3)*pow(n,3)) /* end An */)*cos(n*PI*i/L) + (/* start Bn */ (L*L*((1-2*PI*PI*n*n)*cos(2*PI*n)+2*PI*n*sin(2*PI*n)-1))/(3*pow(PI,3)*pow(n,3)) /* end Bn */)*sin(n*PI*i/L);
+			latestY += (/* start An */ (L*L*((2*M_PI*M_PI*n*n-1)*sin(2*M_PI*n)+2*M_PI*n*cos(2*M_PI*n)))/(2*pow(M_PI,3)*pow(n,3)) /* end An */)*cos(n*M_PI*i/L) + (/* start Bn */ (L*L*((1-2*M_PI*M_PI*n*n)*cos(2*M_PI*n)+2*M_PI*n*sin(2*M_PI*n)-1))/(3*pow(M_PI,3)*pow(n,3)) /* end Bn */)*sin(n*M_PI*i/L);
 		}
 
 		cout << setprecision(20) << i << " " << latestY << endl;
