8e4d44d92b15a999931664cc2b01cc06ad9b9af2
Josh Smith <jdsmit60@users.noreply.github.com>
2017-03-06 00:29:01 -0700

    Now handles new naming scheme
    fileanimator.cpp uses a new data file naming scheme that works well with python's .sort() method for lists. graphall.sh needed to be updated to handle this new scheme.

--- ProofOfConcept/graphall.sh
+++ ProofOfConcept/graphall.sh
@@ -7,6 +7,6 @@ datfiles=(time*.dat)
 index=0
 
 for file in "${datfiles[@]}"; do
-printf "set term pngcairo size 640,480 truecolor enhanced; set output '%s'; set style line 1 lt 1 lw 3 linecolor rgb '#ff0000'; set yrange [%s]; plot '%s' using 1:2 with lines smooth unique ls 1\n" $(printf "plot%s.png" $index) $(cat range.dat) $(printf "time%s.dat" $index) | gnuplot
+printf "set term pngcairo size 640,480 truecolor enhanced; set output '%s'; set style line 1 lt 1 lw 3 linecolor rgb '#ff0000'; set yrange [%s]; plot '%s' using 1:2 with lines smooth unique ls 1\n" $(printf "%s.png" $(echo ${datfiles[index]} | sed s/.dat//g)) $(cat range.dat) ${datfiles[index]} | gnuplot
 index=$(echo "$index + 1" | bc)
 done
