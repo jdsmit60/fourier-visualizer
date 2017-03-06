3656589741f9031078558a223181263140956b6c
Josh Smith <jdsmit60@users.noreply.github.com>
2017-03-06 01:18:21 -0700

    Got rid of old example code

--- ProofOfConcept/simpleplotter.py
+++ ProofOfConcept/simpleplotter.py
@@ -6,19 +6,6 @@ import matplotlib.pyplot as plt
 import re
 import os
 
-# this code used for experiments/understanding how these things work
-#time0datx = np.loadtxt("time0.dat",usecols = 0)
-#time0daty = np.loadtxt("time0.dat",usecols = 1)
-
-#print(time0datx)
-#print(time0daty)
-
-#plt.plot(np.loadtxt("time0.dat",usecols = 0),np.loadtxt("time0.dat",usecols = 1))  # don't need to save columns to variables
-
-#pylab.savefig('pyplot0.png')
-
-# start actual program
-
 datFiles = []
 
 for filename in os.listdir( os.curdir ):
