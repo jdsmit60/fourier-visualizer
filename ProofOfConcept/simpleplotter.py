da3e963e85d0dbeb5b702fc2134de14da8df0295
Josh Smith <jdsmit60@users.noreply.github.com>
2017-03-06 00:37:10 -0700

    Declare new figures for each plot
    No longer plots all of the data on top of each other. Also switched from pylab.savefig to plt.savefig.
    Both of these changes increased performance (the former far more than the latter).

--- ProofOfConcept/simpleplotter.py
+++ ProofOfConcept/simpleplotter.py
@@ -30,7 +30,9 @@ datFiles.sort()
 
 index = 0
 for datFile in datFiles:
+	plt.figure()
 	plt.plot(np.loadtxt(datFile,usecols = 0),np.loadtxt(datFile,usecols = 1))
 	plotname = "pyplot" + str(index) + ".png"
-	pylab.savefig(plotname)
+	plt.savefig(plotname)
+	plt.close()
 	index += 1
