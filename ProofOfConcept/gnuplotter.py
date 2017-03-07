536616ba77fa00b5107f9a71389bbe7a4e68f837
Josh Smith <jdsmit60@users.noreply.github.com>
2017-03-06 22:26:14 -0700

    Outputs files with leading zeros
    Used for more robust operation with other programs and for consistency with the input files

--- ProofOfConcept/gnuplotter.py
+++ ProofOfConcept/gnuplotter.py
@@ -3,6 +3,7 @@
 import re
 import os
 import subprocess
+import math
 
 datFiles = []
 
@@ -18,8 +19,10 @@ datFiles.sort()
 with open("range.dat","r") as rangefile:
 	functionRange = rangefile.read()
 
+digits = math.log10(len(datFiles)) + 1
+
 index = 0
 for datFile in datFiles:
-	command = "gnuplot -e \"set term pngcairo size 640,480 truecolor enhanced; set output\'gnuplot" + str(index) + ".png\'; set style line 1 lt 1 lw 3 linecolor rgb \'#ff0000\'; set yrange [" + functionRange + "]; plot \'" + datFile + "\' using 1:2 with lines smooth unique ls 1\""
+	command = "gnuplot -e \"set term pngcairo size 640,480 truecolor enhanced; set output\'gnuplot" + str(index).zfill(int(digits)) + ".png\'; set style line 1 lt 1 lw 3 linecolor rgb \'#ff0000\'; set yrange [" + functionRange + "]; plot \'" + datFile + "\' using 1:2 with lines smooth unique ls 1\""
 	subprocess.Popen(command, shell=True)
 	index += 1
