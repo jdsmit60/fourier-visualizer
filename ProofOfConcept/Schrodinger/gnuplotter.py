a9c88770b7c1f68980b364afeacf8b38bf24769e
Josh Smith <jdsmit60@users.noreply.github.com>
2017-03-14 16:33:18 -0700

    Prettier Higher-Res Graph, other small tweaks

--- ProofOfConcept/Schrodinger/gnuplotter.py
+++ ProofOfConcept/Schrodinger/gnuplotter.py
@@ -7,7 +7,7 @@ import math
 
 datFiles = []
 
-for filename in os.listdir( os.curdir ):
+for filename in os.listdir( "./datafiles" ):
 	potentialMatch = re.match("time[0-9]*.dat",filename)
 	if(potentialMatch):
 		datFiles.append(potentialMatch.group())
@@ -16,15 +16,17 @@ for filename in os.listdir( os.curdir ):
 
 datFiles.sort()
 
-with open("range.dat","r") as rangefile:
+with open("./datafiles/range.dat","r") as rangefile:
 	functionRange = rangefile.read()
 
 digits = int(math.log10(len(datFiles)) + 1)
 
 index = 0
 for datFile in datFiles:
-	command = "gnuplot -e \"set term pngcairo size 640,480 truecolor enhanced; set output\'gnuplot" + str(index).zfill(digits) + ".png\'; set style line 1 lt 1 lw 3 linecolor rgb \'#ff0000\'; set yrange [" + functionRange + "]; plot \'" + datFile + "\' using 1:2 with lines smooth unique ls 1\""
+	command = "gnuplot -e \"set term pngcairo size 1280,960 truecolor enhanced; set output\'./imagefiles/gnuplot" + str(index).zfill(digits) + ".png\'; set style line 1 lt 1 lw 2 linecolor rgb \'#0099ff\'; set yrange [" + functionRange + "]; set xlabel \'Position x in meters\'; set ylabel \'Probability\'; plot \'./datafiles/" + datFile + "\' using 1:2 with lines smooth unique ls 1\""
 	subprocess.Popen(command, shell=True, stdin=None, stdout=None, stderr=None)
 	index += 1
 
-subprocess.Popen("ffmpeg -i gnuplot%" + str(digits) + "d.png -vcodec h264 -r 25 -crf 18 -pix_fmt yuv420p -y graph.mp4 -nostdin", shell=True)
+# -pix_fmt yuv420p is essential here - otherwise videos will not play in browsers
+# use os.system here so that ffmpeg exits properly - subprocess.Popen was causing issues
+os.system("ffmpeg -i ./imagefiles/gnuplot%" + str(digits) + "d.png -vcodec h264 -r 25 -crf 18 -pix_fmt yuv420p -y graph.mp4 -nostdin")
